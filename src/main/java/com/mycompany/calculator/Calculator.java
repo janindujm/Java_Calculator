86980dce6df5c11fd672697ba1d550ef6a2a3f56
Janindu <janindujm@gmail.com>
2024-10-14 12:35:51 +0530

    initial commit

---
+++ src/main/java/com/mycompany/calculator/Calculator.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculator;
+
+/**
+ *
+ * @author janin
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        java.awt.EventQueue.invokeLater(() -> new NewJFrame().setVisible(true));
+    }
+}
